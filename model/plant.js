efb5894043aa6bfbca6f8cea3b98a9471b755de9
C-Kristel <85912319+C-Kristel@users.noreply.github.com>
2022-05-05 23:00:19 +0800

    edit plant schema

--- model/plant.js
+++ model/plant.js
@@ -2,26 +2,29 @@ const mongoose = require('mongoose')
 
 const PlantSchema = new mongoose.Schema(
 	{
-		plantNum: {
-            type: String,
-            required: true
-        },
-        plantHeight: { 
-            type: [String], 
-            required: true
-        },
-		numberOfLeaves: { 
-            type: [String], 
-            required: true 
-        },
-		leafGreenness: { 
-            type: [String], 
-            required: true 
-        },
-        date: {
-            type: Date,
-            default: Date.now
-        }    
+        plant: [{
+            plantNum: {
+                type: String,
+                required: true
+            },
+            plantHeight: { 
+                type: String, 
+                required: true
+            },
+            numberOfLeaves: { 
+                type: String, 
+                required: true 
+            },
+            leafGreenness: { 
+                type: String, 
+                required: true 
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }    
+        }]
+		
 	},
 	{ collection: 'plant' }
 )
