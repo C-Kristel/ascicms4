b356483552edd0801f92361bbce9660e48c133b5
C-Kristel <85912319+C-Kristel@users.noreply.github.com>
2022-05-05 23:16:08 +0800

    dunno anymore

--- model/plant.js
+++ model/plant.js
@@ -2,29 +2,26 @@ const mongoose = require('mongoose')
 
 const PlantSchema = new mongoose.Schema(
 	{
-        plant: [{
-            plantNum: {
-                type: String,
-                required: true
-            },
-            plantHeight: { 
-                type: String, 
-                required: true
-            },
-            numberOfLeaves: { 
-                type: String, 
-                required: true 
-            },
-            leafGreenness: { 
-                type: String, 
-                required: true 
-            },
-            date: {
-                type: Date,
-                default: Date.now
-            }    
-        }]
-		
+        plantNum: {
+            type: String,
+            required: true
+        },
+        plantHeight: { 
+            type: [String], 
+            required: true
+        },
+        numberOfLeaves: { 
+            type: [String], 
+            required: true 
+        },
+        leafGreenness: { 
+            type: [String], 
+            required: true 
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        }   
 	},
 	{ collection: 'plant' }
 )
