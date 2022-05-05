05c6f2ff3cca47d631a2ea590cdbc3b73d225333
C-Kristel <85912319+C-Kristel@users.noreply.github.com>
2022-05-06 03:10:30 +0800

    dsfa

--- routes/User.js
+++ routes/User.js
@@ -59,7 +59,7 @@ router.post('/login', async (req, res) => {
 			JWT_SECRET
 		)
 
-		return res.json({ status: 'ok', data: token, message: 'User logged in successfully'})
+		return res.json({ status: 'ok', "auth-token": token, message: 'User logged in successfully'})
 	}
 
 	res.json({ status: 'error', error: 'Invalid username/password' })
