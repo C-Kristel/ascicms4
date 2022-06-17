afef401d1bdcd1a0d18ecb5eff786ea90c9808be
C-Kristel <85912319+C-Kristel@users.noreply.github.com>
2022-06-17 11:35:49 +0800

    fix ph Schema

--- model/pH.js
+++ model/pH.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
-const ParamsSchema = new mongoose.Schema(
+const pH_lvl = new mongoose.Schema(
 	{
 		pH_lvl: {type: String, required: true}
 	},
 	{ collection: 'PH_Level' }
 )
 
-const model = mongoose.model('ParamsSchema', ParamsSchema)
+const model = mongoose.model('pH', pH_lvl)
 
 module.exports = model
