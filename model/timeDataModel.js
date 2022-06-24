fffa8cbc69d9f6273df8ecc802fbfe95341e4502
UnknownVII <85911718+UnknownVII@users.noreply.github.com>
2022-06-24 14:06:03 +0800

    Update timeDataModel.js

--- model/timeDataModel.js
+++ model/timeDataModel.js
@@ -4,7 +4,7 @@ const TimeDataSchema = new mongoose.Schema(
     {
         start_time: {type: String, required: false},
         end_time: {type: String, required: false},
-        duration: {type: String, required: false},
+        duration: {type: String, required: false}
     },
     { collection: 'TimeData' }
 )
